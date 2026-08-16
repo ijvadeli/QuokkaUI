f0933013a1128c2fac21c37826822cda436cbe55
ijvadeli <idhvandelindt@icloud.com>
2026-08-16 13:37:18 +0200

    Updated website

--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Layout>
       <div className="bg-amber-400 p-2 flex items-center">
-        <img src="favicon.svg" className="h-25"/>
-        <Typography type="h1">
-          QuokkaUI is still W.I.P<br/>No proper website just showcase.
+        <img src="favicon.svg" className="h-10 pr-4"/>
+        <Typography type="p" className="font-monospace font-bold">
+          QuokkaUI is still W.I.P - No proper website, just showcase.
         </Typography>
       </div>
       <Buttons />
